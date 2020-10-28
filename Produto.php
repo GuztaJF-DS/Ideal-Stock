6a8d046cea3560d405a86d76458e11ab717ac68b
devLeSilverio <leticia.silverio09@gmail.com>
2020-10-28 09:59:19 -0300

    updating the font-awesome

--- Produto.php
+++ Produto.php
@@ -11,7 +11,7 @@ session_start();
     <script src="Js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="Css/style.css">
   <link rel="stylesheet" href="Css/bootstrap.min.css">
-  <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
 
 </head>
 <body style="background-color:#F0FDFF;">
--- stock.php
+++ stock.php
@@ -8,7 +8,7 @@
     <script src="Js/script.js"></script>
     <link rel="stylesheet" href="Css/style.css" />
     <link rel="stylesheet" href="Css/bootstrap.min.css" />
-    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
   </head>
 
   <body style="background-color: #F0FDFF">
